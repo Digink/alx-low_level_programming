aa531ac8972eb4039e01bee7fb8f637732357a88
Digink <uwadinachinkiruka@gmail.com>
2023-09-12 18:25:49 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - a dog's basic info
+ *@name:The name of the dog
+ *@age:The age of the dog
+ *@owner: The dogs owner
+ *
+ *Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
