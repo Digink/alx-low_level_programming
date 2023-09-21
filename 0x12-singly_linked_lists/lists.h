f89c6aa4e9027592f5b85b13de0f606b939ffab8
Digink <uwadinachinkiruka@gmail.com>
2023-09-21 04:51:26 -0700

    headerfile

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,19 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+typedef struct list_s
 {
-	return (write(1, &c, 1));
-}
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
